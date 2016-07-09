ff95c96709ccca31246a57514e4889c959932509
Nishant Agrawal <Nishi.in@gmail.com>
2016-07-09 12:00:47 +0530

    Add jshint watch to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,33 @@
 /**
  * Created by nishantagrawal on 7/9/2016.
  */
-var gulp=require('gulp')
-gulp.task('test',function()
-{
-    console.log('Test Task done.')
-}
+var gulp = require('gulp')
+var gulpUtil = require('gulp-util')
+var jshint = require('gulp-jshint');
+
+
+gulp.task('test', function () {
+        console.log('Test Task done.')
+    }
 )
+
+gulp.task('copyhtml', function () {
+    gulp.src('src/*.html').pipe(gulp.dest('build'));
+    gulpUtil.log('Source HTML copied.');
+
+})
+
+gulp.task('watch', function () {
+    gulp.watch('src/js/*.js', ['jshint']);
+
+});
+
+
+gulp.task('jshint', function () {
+    return gulp.src('src/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+
+gulp.task('j')
---
+++ src/js/test.js
@@ -0,0 +1,6 @@
+/**
+ * Created by nishantagrawal on 7/9/2016.
+ */
+
+/** Holo!*/
+var a = 2;
