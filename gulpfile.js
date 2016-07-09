37e1266f83b305b235a3cf69a2bc865ae59c5ae5
Nishant Agrawal <Nishi.in@gmail.com>
2016-07-09 12:04:18 +0530

    fix error

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,21 @@
-/**
- * Created by nishantagrawal on 7/9/2016.
- */
+
 var gulp = require('gulp')
 var gulpUtil = require('gulp-util')
-var jshint = require('gulp-jshint');
+var jshint = require('gulp-jshint')
 
-
-gulp.task('test', function () {
-        console.log('Test Task done.')
-    }
-)
-
-gulp.task('copyhtml', function () {
-    gulp.src('src/*.html').pipe(gulp.dest('build'));
-    gulpUtil.log('Source HTML copied.');
-
+gulp.task('test', function(){
+    console.log('Test task');
 })
 
-gulp.task('watch', function () {
-    gulp.watch('src/js/*.js', ['jshint']);
+gulp.task('copyhtml', function(){
+    gulpUtil.log('Run copy html');
+    gulp.src('src/*.html').pipe(gulp.dest('build'));
+})
 
-});
+gulp.task('watch', function(){
+    gulp.watch('src/js/*.js',['jshint'])
+})
 
-
-gulp.task('jshint', function () {
-    return gulp.src('src/js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish'));
-});
-
-
-gulp.task('j')
+gulp.task('jshint', function(){
+    return gulp.src('src/js/*.js').pipe(jshint()).pipe(jshint.reporter('jshint-stylish'));
+})
